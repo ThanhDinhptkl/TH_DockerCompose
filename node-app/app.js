52d03a974e304f0c79c733118d251509b8f2e89a
ThanhDinhptkl <ntdinh25@gmail.com>
2025-04-11 17:57:56 +0700

    update file

--- node-app/app.js
+++ node-app/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Xin chào từ ứng dụng Node.js trong Docker!");
+  res.send("Hello from Docker + Node.js!");
 });
 
-app.listen(port, () => {
-  console.log(`Ứng dụng chạy tại http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
